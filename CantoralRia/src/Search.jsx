abcc57b28b97e3a1f33d34f739c237630265394e
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2023-05-06 23:26:39 +0200

    Iterable list on search component

--- CantoralRia/src/Search.jsx
+++ CantoralRia/src/Search.jsx
@@ -6,8 +6,8 @@ import { Searchbar, List } from 'react-native-paper';
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = React.useState('');
-
     const onChangeSearch = query => setSearchQuery(query);
+    const songItems = Array(10).fill(null);
 
     return (
         <View style={{ flex: 1 }}>
@@ -19,97 +19,16 @@ const Search = () => {
             />
             <ScrollView style={{ height: '100%' }}>
                 <List.Section>
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
-                    <List.Item
-                        title="Nombre canto"
-                        description="Letra de la canción, previsualización de la canción, etc..."
-                        onPress={() => { console.log('Pressed'); }}
-                        left={props => <List.Icon {...props} icon="music" />}
-                        right={props => <List.Icon {...props} icon="chevron-right" />}
-                    />
+                    {songItems.map((item, index) => ( // Iterar sobre el array y crear un List.Item para cada elemento
+                        <List.Item
+                            key={`song-id ${index}`}
+                            title="Nombre canto"
+                            description="Letra de la canción, previsualización de la canción, etc..."
+                            onPress={() => { console.log('Pressed'); }}
+                            left={props => <List.Icon {...props} icon="music" />}
+                            right={props => <List.Icon {...props} icon="chevron-right" />}
+                        />
+                    ))}
                 </List.Section>
 
             </ScrollView>
