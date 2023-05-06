924ff459e348ff51462abd114f8da008146cb0e4
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2023-05-06 23:26:59 +0200

    Added Song pre-component

---
+++ CantoralRia/src/Song.jsx
@@ -0,0 +1,24 @@
+//import react native
+import React from 'react';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+
+
+const Song = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <ScrollView style={{ height: '100%' }}>
+                <Text style={songStyles.text}>Resultados de búsqueda</Text>
+            </ScrollView>
+        </View>
+    );
+};
+
+const songStyles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 10
+    }
+});
+
+export default Song;
